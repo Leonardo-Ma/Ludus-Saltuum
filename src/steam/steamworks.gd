911e5de682ee016898de3a09aeadeea1b9b5c62a
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-07-30 17:09:16 -0300

    Feat: Finish car level
    Fix: Cave meshes using single collision for floor
    
    Chore: Add steamworks TODO to improve ownership check

--- src/steam/steamworks.gd
+++ src/steam/steamworks.gd
@@ -8,6 +8,12 @@ func _ready() -> void:
 		push_error("Steam not running")
 		return
 
+	# TODO There are better ways to check as the game could have been family shared or free weekend...
+	#var is_valid_license: bool = Steam.userHasLicenseForApp(Steam.getSteamID(), Steam.getAppID())
+	#if !is_valid_license:
+	#	push_error("Player didn't purchase the game")
+	#	return
+
 	var steam_player_name: String = Steam.getPersonaName()
 	print("Username: ", steam_player_name + "\n")
 
