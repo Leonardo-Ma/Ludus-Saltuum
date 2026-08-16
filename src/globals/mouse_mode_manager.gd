9a6ad25560c9c79e3aa2a7b6e29ce42d5d03b3ad
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-08-16 19:54:38 -0300

    Refactor: Turn MouseModeManager into static class instead of autoload

--- src/globals/mouse_mode_manager.gd
+++ src/globals/mouse_mode_manager.gd
@@ -1,25 +1,27 @@
 ## Centralized mouse mode arbitration via named, priority-ordered requests
+class_name MouseModeManager
 extends Node
 
 ## Highest priority first; first source with an active request wins
 const _PRIORITY: Array[StringName] = [&"popup", &"shop", &"menu", &"gameplay", &"device"]
 
-var _requests: Dictionary[StringName, Input.MouseMode] = {}
+static var _requests: Dictionary[StringName, Input.MouseMode] = {}
 
 
-func request_mode(source: StringName, mode: Input.MouseMode) -> void:
+static func request_mode(source: StringName, mode: Input.MouseMode) -> void:
 	assert(source in _PRIORITY, "MouseModeManager: unknown source '%s', add it to _PRIORITY" % source)
 	_requests[source] = mode
 	_apply()
 
 
-func release(source: StringName) -> void:
+static func release(source: StringName) -> void:
 	_requests.erase(source)
 	_apply()
 
 
-func _apply() -> void:
+static func _apply() -> void:
 	for source: StringName in _PRIORITY:
 		if _requests.has(source):
 			Input.mouse_mode = _requests[source]
 			return
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
