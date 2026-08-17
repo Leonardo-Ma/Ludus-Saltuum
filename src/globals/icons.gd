c8f54e72e432dbc1c0503126173642df149dad7a
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-08-17 17:22:48 -0300

    Feat: Add icons class with textures as consts

---
+++ src/globals/icons.gd
@@ -0,0 +1,13 @@
+## Reusable icons const
+class_name Icons
+
+const CHARACTER_MOVEMENT: CompressedTexture2D = preload("uid://d4g1stey2kdtm")
+const ARROW_LEFT: CompressedTexture2D = preload("uid://btsgs5x0kxkfs")
+const ARROW_RIGHT: CompressedTexture2D = preload("uid://6ytvlaq1ur6c")
+const ARROW_DOWN: CompressedTexture2D = preload("uid://cahb5jj7i2am0")
+const ARROW_UP: CompressedTexture2D = preload("uid://c1xeqab3tfrlw")
+const JUMP: CompressedTexture2D = preload("uid://01223baevlix")
+const ATTACK: CompressedTexture2D = preload("uid://cdm1p42bwmr10")
+const FLAG: CompressedTexture2D = preload("uid://bf5rf38myhsic")
+const UNDO: CompressedTexture2D = preload("uid://lhp1fou4j25j")
+const EYE: CompressedTexture2D = preload("uid://by042xkk35f0m")
