9e023741ea91d72778597155e958cb3169976316
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-08-18 12:39:22 -0300

    Fix: Remove hardcoded steam appids

--- src/steam/steamworks.gd
+++ src/steam/steamworks.gd
@@ -8,6 +8,12 @@ func _ready() -> void:
 		push_error("Steam not running")
 		return
 
+	if OS.is_debug_build():
+		if OS.has_feature("demo"):
+			Steam.steamInitEx(5131920)
+		else:
+			Steam.steamInitEx(4832410)
+
 	# TODO There are better ways to check as the game could have been family shared or free weekend...
 	#var is_valid_license: bool = Steam.userHasLicenseForApp(Steam.getSteamID(), Steam.getAppID())
 	#if !is_valid_license:
