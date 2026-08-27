d6e8309870206c8421a1532c641ce81f7d84c793
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-08-27 18:58:55 -0300

    Refactor: Simplify WorldSaveController, turning into autoload
    Was a static singleton shenaningans

--- src/world_save_controller.gd
+++ src/world_save_controller.gd
@@ -1,79 +1,53 @@
 # TODO Consider a different approach?
-# TODO Probably is best an autoload
-## Tracks world state during gameplay (consumed collectibles, killed enemies)
-## Used by SaveManager during save/load operations
-class_name WorldSaveController
-extends RefCounted
+## Tracks consumed collectibles / killed enemies for save/load
+extends Node
 
 const _COLLECTIBLE_MATCH_SQ: float = 0.25
 const _ENEMY_MATCH_SQ: float = 0.25
 
-static var _consumed_collectible_positions: Array[Vector3] = []
-static var _killed_enemy_positions: Array[Vector3] = []
-static var _is_initialized: bool = false
+var _consumed_collectible_positions: Array[Vector3] = []
+var _killed_enemy_positions: Array[Vector3] = []
 
 
-static func _get_tree() -> SceneTree:
-	return Engine.get_main_loop() as SceneTree
-
-
-static func _ensure_initialized() -> void:
-	if _is_initialized:
-		return
-	_is_initialized = true
+func _ready() -> void:
 	GameEvents.collectible_consumed.connect(_on_collectible_consumed)
 	GameEvents.enemy_killed.connect(_on_enemy_killed)
-	ApplicationStateManager.main_menu_requested.connect(_cleanup)
-
-
-static func _cleanup() -> void:
-	if not _is_initialized:
-		return
-	_is_initialized = false
-	GameEvents.collectible_consumed.disconnect(_on_collectible_consumed)
-	GameEvents.enemy_killed.disconnect(_on_enemy_killed)
-	ApplicationStateManager.main_menu_requested.disconnect(_cleanup)
+	ApplicationStateManager.main_menu_requested.connect(reset_data)
 
 
 ## Builds world data for saving
 ## @param data WorldSaveData resource to populate
-static func build_save(data: WorldSaveData) -> void:
-	_ensure_initialized()
+func build_save(data: WorldSaveData) -> void:
 	data.collected_collectible_positions = _consumed_collectible_positions.duplicate()
 	data.killed_enemy_positions = _killed_enemy_positions.duplicate()
 
 
 ## Applies world data when loading a save
 ## @param data WorldSaveData resource to apply
-static func apply_save(data: WorldSaveData) -> void:
-	_ensure_initialized()
+func apply_save(data: WorldSaveData) -> void:
 	_consumed_collectible_positions = data.collected_collectible_positions.duplicate()
 	_killed_enemy_positions = data.killed_enemy_positions.duplicate()
-
 	_disable_killed_enemies.call_deferred()
 	_disable_consumed_collectibles.call_deferred()
 
 
-## Resets world state for new game
-static func reset_data() -> void:
-	_ensure_initialized()
+func reset_data() -> void:
 	_consumed_collectible_positions.clear()
 	_killed_enemy_positions.clear()
 
 
-static func _on_collectible_consumed(pos: Vector3) -> void:
+func _on_collectible_consumed(pos: Vector3) -> void:
 	_consumed_collectible_positions.append(pos)
 
 
-static func _on_enemy_killed(pos: Vector3) -> void:
+func _on_enemy_killed(pos: Vector3) -> void:
 	_killed_enemy_positions.append(pos)
 
 
 # TODO BUG Improve this garbage
-static func _disable_consumed_collectibles() -> void:
-	var tree: SceneTree = _get_tree()
+func _disable_consumed_collectibles() -> void:
 	for pos: Vector3 in _consumed_collectible_positions:
-		for c: Node in tree.get_nodes_in_group(Groups.COLLECTIBLES):
+		for c: Node in get_tree().get_nodes_in_group(Groups.COLLECTIBLES):
 			var collectible: Collectible = c as Collectible
 			if collectible == null:
 				continue
@@ -83,10 +57,9 @@ static func _disable_consumed_collectibles() -> void:
 
 
 # TODO BUG Improve this garbage
-static func _disable_killed_enemies() -> void:
-	var tree: SceneTree = _get_tree()
+func _disable_killed_enemies() -> void:
 	for pos: Vector3 in _killed_enemy_positions:
-		for enemy: Node in tree.get_nodes_in_group(Groups.ENEMIES):
+		for enemy: Node in get_tree().get_nodes_in_group(Groups.ENEMIES):
 			var entity: AggressiveEntity = enemy as AggressiveEntity
 			if entity == null:
 				continue
