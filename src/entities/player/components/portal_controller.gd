c52588d1c88c62f3da7d49ae69aa33267fbcf961
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-09-10 15:08:58 -0300

    Refactor: Move most features from assets/ to mechanics/ in environment/
    Remove: Unused teleporter asset
    
    Rename TeleportPortal to Portal

--- src/entities/player/components/portal_controller.gd
+++ src/entities/player/components/portal_controller.gd
@@ -4,7 +4,7 @@ extends Node
 @onready var _portal_transitioning: bool = false
 
 
-func begin_portal_transition(destination: TeleportPortal, actor: Node3D) -> void:
+func begin_portal_transition(destination: Portal, actor: Node3D) -> void:
 	if _portal_transitioning or destination.is_disabled:
 		return
 
