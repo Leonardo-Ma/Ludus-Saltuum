c4eed6e1c1e18c007e17b8153acd67ff5f94f14d
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-09-10 23:33:45 -0300

    Chore: Fix skip level buttons to extend UIButton
    Both files are empty, but they now have same effects

---
+++ src/ui/popups/skip_level_confirm/yes_button.gd
@@ -0,0 +1,11 @@
+## Empty on purpose to extend UIButton. So it applies effects.
+## Logic is in owner
+extends UIButton
+
+
+func _button_ready() -> void:
+	pass
+
+
+func _button_pressed() -> void:
+	pass
