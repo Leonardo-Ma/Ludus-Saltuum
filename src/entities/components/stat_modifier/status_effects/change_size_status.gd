25e062219064701086f598831d21672ceff9ff54
Leonardo <68714883+Leonardo-Ma@users.noreply.github.com>
2026-09-13 21:04:37 -0300

    Feat: Add speed and jump multiplier for change size status
    When shrink or gigantify it adapts speed and jump

--- src/entities/components/stat_modifier/status_effects/change_size_status.gd
+++ src/entities/components/stat_modifier/status_effects/change_size_status.gd
@@ -4,6 +4,9 @@ extends StatusEffect
 
 @export_range(0.01, 100.0, 0.01) var scale_factor: float = 10.0
 
+@export_range(0.01, 10.0, 0.01) var speed_multiplier: float = 1.0
+@export_range(0.01, 10.0, 0.01) var jump_multiplier: float = 1.0
+
 
 func get_id() -> StringName:
 	return &"change_size"
@@ -18,6 +21,8 @@ func on_apply(_target: Node) -> void:
 	if player and not _target.has_meta("change_size_applied"):
 		var tween: Tween = player.get_tree().create_tween()
 		tween.tween_property(player, "scale", player.scale * scale_factor, 1.0)
+		player.movement.speed *= speed_multiplier
+		player.movement.jump_velocity *= jump_multiplier
 		_target.set_meta("change_size_applied", true)
 
 
@@ -26,4 +31,6 @@ func on_remove(_target: Node) -> void:
 	if player and _target.has_meta("change_size_applied"):
 		var tween: Tween = player.get_tree().create_tween()
 		tween.tween_property(player, "scale", player.scale / scale_factor, 1.0)
+		player.movement.speed /= speed_multiplier
+		player.movement.jump_velocity /= jump_multiplier
 		_target.remove_meta("change_size_applied")
